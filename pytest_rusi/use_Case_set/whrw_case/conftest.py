225c5db76fa9e9081553f43684d7fecae5b77d98
lupingp <294402584@qq.com>
2018-12-13 16:04:35 +0800

    增加了 外呼任务 模块 测试代码

---
+++ pytest_rusi/use_Case_set/whrw_case/conftest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/13  14:53
+# @Author  : 陆平！！
+# @FileName: conftest.py
+# @Software: PyCharm
+
+import pytest
+from pytest_rusi.page.whrw_page import _login_whrw_1,_login_whrw_2
+from pytest_rusi.page.login_page import _login_One
+
+@pytest.fixture(scope="function")
+def login_whrw_1(driver):
+    _login_whrw_1(driver)
+
+@pytest.fixture(scope="function")
+def login_whrw_2(driver):
+    _login_whrw_2(driver)
+
+@pytest.fixture(scope="function")
+def login_One(driver,host):
+    _login_One(driver,host)
